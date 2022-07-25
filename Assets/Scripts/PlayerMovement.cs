65364f315a7c15036073969488b41eafe91893fc
Yankı Ufuk Dülger <yankiudulger@gmail.com>
2022-07-25 22:56:03 +0300

    pt3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
 
-    // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; 
+
+    }
+    void FixedUpdate()
+    {
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+
     }
 }
